1a9c7635e27b4b76a196308476b3c6e9df5df2fc
05GoodeA <aarongoode4029@gmail.com>
2020-10-25 12:39:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+#Topic Select Control Panel
+
+topic_choice = 0
+
+
+
+print("Hello, and welcome to the Physics Equations Calculator.\nPlease select a topic:")
+print("1) Electricity")
+topic_choice = str(input("Pick a Topic:"))
+
+
+
+
+if topic_choice == "1":
+    print("Hello")
