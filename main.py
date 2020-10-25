cb986cb8dbc0031fb6a935e0c4e3dd70a53a5356
05GoodeA <aarongoode4029@gmail.com>
2020-10-25 14:01:13 +0000

    finished the V = IR equation

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 
-#Topic Select Control Panel
+# Topic Select Control Panel
 
-topic_choice = 0
-
-
-
-print("Hello, and welcome to the Physics Equations Calculator.\nPlease select a topic:")
+print("Hello, and welcome to the Physics Equations Calculator:")
 print("1) Electricity")
 topic_choice = str(input("Pick a Topic:"))
-
-
-
 
 if topic_choice == "1":
     print("Electricity Equations:")
@@ -20,4 +13,24 @@ if topic_choice == "1":
     topic1_equations = str(input("Choose an Equation:"))
 
     if topic1_equations == "1":
-        
+        print("What do you want to work out:")
+        print("1) Voltage")
+        print("2) Current")
+        print("3) Resistance")
+        topic1_equations1 = str(input("Choose what you want to work out:"))
+
+        if topic1_equations1 == "1":
+            topic1_equations1_current = int(input("Enter the current:"))
+            topic1_equations1_resistance = int(input("Enter the resistance:"))
+            topic1_equations1_voltage = topic1_equations1_current*topic1_equations1_resistance
+            print("The voltage is:", topic1_equations1_voltage)
+        elif topic1_equations1 == "2":
+            topic1_equations1_resistance = int(input("Enter the resistance:"))
+            topic1_equations1_voltage = int(input("Enter the voltage:"))
+            topic1_equations1_current = topic1_equations1_voltage/topic1_equations1_resistance
+            print("The current is", topic1_equations1_current)
+        elif topic1_equations1 == "3":
+            topic1_equations1_voltage = int(input("Enter the voltage:"))
+            topic1_equations1_current = int(input("Enter the current:"))
+            topic1_equations1_resistance = topic1_equations1_voltage/topic1_equations1_current
+            print("The resistance is", topic1_equations1_resistance)
